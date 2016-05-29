ec6a74b4804823774e0612b8f4a3a2d571b5092f
littleStar711 <wangx201401@126.com>
2016-05-29 13:49:38 +0800

    Initial Commit

---
+++ HybridCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HybridCpp
+//
+//  Created by Wang on 16/5/29.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
